b4926eed3e267447a81088672301fff3c4d79a77
gombasan <xogmlwkdi@naver.com>
2024-05-27 22:13:42 +0000

    5월 28일 최소 직사각형 문제 풀이

---
+++ src/day09/MinimumRectangle.java
@@ -0,0 +1,19 @@
+package day09;
+
+public class MinimumRectangle {
+
+	public int solution(int[][] sizes) {
+		int maxWidth = 0;
+		int maxHeight = 0;
+
+		for (int[] size : sizes) {
+			int width = Math.max(size[0], size[1]);
+			int height = Math.min(size[0], size[1]);
+
+			maxWidth = Math.max(maxWidth, width);
+			maxHeight = Math.max(maxHeight, height);
+		}
+
+		return maxWidth * maxHeight;
+	}
+}
